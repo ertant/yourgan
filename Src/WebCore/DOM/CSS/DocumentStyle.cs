57192d48781c6230e8f9c874c9715070a8fb32f8
ertan.tike@gmail.com <ertan.tike@gmail.com@5af1dcd4-2534-11de-b09f-0fcc949ee51a>
2010-01-10 22:27:15 +0000

    initial css dom classes

---
+++ Src/WebCore/DOM/CSS/DocumentStyle.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Yourgan.Core.DOM.CSS
+{
+    public class DocumentStyle
+    {
+        public StyleSheetList StyleSheets
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public string SelectedStyleSheetSet
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public string LastStyleSheetSet
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public string PreferredStyleSheetSet
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public string StyleSheetSets
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public void EnableStyleSheetsForSet(string name)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Src/WebCore/DOM/CSS/StyleSheet.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Yourgan.Core.DOM.CSS
+{
+    public class StyleSheet
+    {
+        private string type;
+
+        public string Type
+        {
+            get
+            {
+                return type;
+            }
+        }
+
+        private string href;
+
+        public string HRef
+        {
+            get
+            {
+                return href;
+            }
+        }
+
+        public Node OwnerNode
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public StyleSheet ParentStyleSheet
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public string Title
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public MediaList Media
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        private bool disabled;
+
+        public bool Disabled
+        {
+            get
+            {
+                return disabled;
+            }
+            set
+            {
+                disabled = value;
+            }
+        }
+    }
+}
