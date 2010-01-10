2e3130caa8c5924ca378f5ad29bbee29a5e9d7d2
ertan.tike@gmail.com <ertan.tike@gmail.com@5af1dcd4-2534-11de-b09f-0fcc949ee51a>
2010-01-10 22:27:15 +0000

    initial css dom classes

---
+++ trunk/Src/WebCore/DOM/CSS/MediaList.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Yourgan.Core.DOM.CSS
+{
+    class MediaList
+    {
+        public string MediaText
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public ulong Length
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public string this[ulong index]
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public void AppendMedium(string medium)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteMedium(string medium)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
